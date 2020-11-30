9073e67bd277bb231d29d571e373ac725e25e5cf
Siarhei Charniak <siarhei_charniak@epam.com>
2020-11-30 12:51:08 +0300

    MODRS-1 - Project Setup: mod-remote-storage

--- src/main/java/rs/ModRemoteStorageApplication.java
+++ src/main/java/rs/ModRemoteStorageApplication.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class ModRemoteStorageApplication {
   public static void main(String[] args) {
-    SpringApplication.run(new Class[]{ ModRemoteStorageApplication.class }, args);
+    SpringApplication.run(ModRemoteStorageApplication.class, args);
   }
 }
---
+++ src/main/java/rs/controller/TenantController.java
@@ -0,0 +1,52 @@
+package rs.controller;
+
+import liquibase.exception.LiquibaseException;
+import lombok.extern.log4j.Log4j2;
+import lombok.var;
+import org.folio.spring.FolioExecutionContext;
+import org.folio.spring.liquibase.FolioSpringLiquibase;
+import org.folio.tenant.domain.dto.TenantAttributes;
+import org.folio.tenant.rest.resource.TenantApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@Log4j2
+@RestController("folioTenantController")
+@RequestMapping(value = "/_/")
+public class TenantController implements TenantApi {
+
+  private final FolioSpringLiquibase folioSpringLiquibase;
+
+  private final FolioExecutionContext context;
+
+  @Autowired
+  public TenantController(FolioSpringLiquibase folioSpringLiquibase,
+                            FolioExecutionContext context) {
+    this.folioSpringLiquibase = folioSpringLiquibase;
+    this.context = context;
+  }
+
+  @Override
+  public ResponseEntity<String> postTenant(@Valid TenantAttributes tenantAttributes) {
+    if (folioSpringLiquibase != null) {
+      var tenantId = context.getTenantId();
+
+      var schemaName = context.getFolioModuleMetadata().getDBSchemaName(tenantId);
+
+      folioSpringLiquibase.setDefaultSchema(schemaName);
+      try {
+        folioSpringLiquibase.performLiquibaseUpdate();
+      } catch (LiquibaseException e) {
+        e.printStackTrace();
+        log.error("Liquibase error", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Liquibase error: " + e.getMessage());
+      }
+    }
+    return ResponseEntity.ok().body("true");
+  }
+}

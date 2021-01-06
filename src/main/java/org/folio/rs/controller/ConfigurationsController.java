44f3862e4391a2a6ed135d685de1125695fdcd78
Vitalii Zahoruiko <vitalii_zahoruiko@epam.com>
2021-01-06 15:41:42 +0200

    MODRS-15 - API for list of providers

--- src/main/java/org/folio/rs/controller/ConfigurationsController.java
+++ src/main/java/org/folio/rs/controller/ConfigurationsController.java
@@ -2,37 +2,37 @@ package org.folio.rs.controller;
 
 import static java.util.Objects.isNull;
 
-import lombok.extern.log4j.Log4j2;
-import org.folio.rs.domain.dto.StorageConfiguration;
-import org.folio.rs.domain.dto.StorageConfigurations;
-import org.folio.rs.rest.resource.ConfigurationsApi;
-import org.folio.rs.service.ConfigurationsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.folio.rs.domain.dto.Provider;
+import org.folio.rs.domain.dto.StorageConfiguration;
+import org.folio.rs.domain.dto.StorageConfigurations;
+import org.folio.rs.rest.resource.ConfigurationsApi;
+import org.folio.rs.service.ConfigurationsService;
+import org.folio.rs.service.ProviderService;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/remote-storage/")
 public class ConfigurationsController implements ConfigurationsApi {
   private static final String CONFIGURATION_NOT_FOUND = "Configuration not found";
 
   private final ConfigurationsService configurationsService;
-
-  @Autowired
-  public ConfigurationsController(ConfigurationsService configurationsService) {
-    this.configurationsService = configurationsService;
-  }
+  private final ProviderService providerService;
 
   @Override
   public ResponseEntity<String> deleteConfigurationById(String configId) {
@@ -69,5 +69,11 @@ public class ConfigurationsController implements ConfigurationsApi {
   @ExceptionHandler({EmptyResultDataAccessException.class, EntityNotFoundException.class})
   public ResponseEntity<String> handleNotFoundExceptions() {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(CONFIGURATION_NOT_FOUND);
+  }
+
+  @RequestMapping("/providers")
+  @ResponseBody
+  public ResponseEntity<List<Provider>> getProviders() {
+    return new ResponseEntity<>(providerService.getProviders(), HttpStatus.OK);
   }
 }

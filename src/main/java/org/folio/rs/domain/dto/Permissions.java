d7ef9c558b94156c838b5a69ccc2ce475054e9c8
khandramai <Viachaslau_Khandramai@epam.com>
2021-01-27 13:51:01 +0300

    MODRS-4 - Refactoring (after build failure)

--- src/main/java/org/folio/rs/domain/dto/Permissions.java
+++ src/main/java/org/folio/rs/domain/dto/Permissions.java
@@ -10,5 +10,5 @@ import java.util.List;
 public class Permissions {
   private String id;
   private String userId;
-  private List<String> listOfPermissions;
+  private List<String> permissions;
 }
--- src/main/resources/db/changelog/changes/initial_schema.sql
+++ src/main/resources/db/changelog/changes/initial_schema.sql
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS system_user_parameters
     id          UUID PRIMARY KEY,
     username    VARCHAR(50) NOT NULL,
     password    VARCHAR(50) NOT NULL,
-    okapi_token VARCHAR(100),
+    okapi_token VARCHAR(8000),
     okapi_url   VARCHAR(100),
     tenant_id   VARCHAR(100)
 );

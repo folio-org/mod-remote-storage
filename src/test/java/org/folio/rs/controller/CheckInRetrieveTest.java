158c651e5a80cd0a2cde2cbec94b8dc0ad297814
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2021-02-11 18:06:12 +0300

    MODRS-11 - test bad request for invalid remote storage id

--- src/test/java/org/folio/rs/controller/CheckInRetrieveTest.java
+++ src/test/java/org/folio/rs/controller/CheckInRetrieveTest.java
@@ -8,19 +8,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CheckInRetrieveTest extends TestBase {
 
   private static final String CHECK_IN_URL = "http://localhost:%s/remote-storage/retrieve/%s/checkInItem";
   private static final String REMOTE_STORAGE_CONFIGURATION_ID = "de17bad7-2a30-4f1c-bee5-f653ded15629";
   private static final String FOLIO_LOCATION_ID = "53cf956f-c1df-410b-8bea-27f712cca7c0";
+  private static final String REMOTE_STORAGE_ERROR_CONFIGURATION_ID = "de17bad7-2a30-4f1c-bee5-f653ded15628";
 
   private String checkInUrl;
+  private String errorCheckInUrl;
 
   @Autowired
   private LocationMappingsRepository locationMappingsRepository;
@@ -32,6 +37,7 @@ public class CheckInRetrieveTest extends TestBase {
     locationMapping.setConfigurationId(UUID.fromString(REMOTE_STORAGE_CONFIGURATION_ID));
     locationMappingsRepository.save(locationMapping);
     checkInUrl = String.format(CHECK_IN_URL, okapiPort, REMOTE_STORAGE_CONFIGURATION_ID);
+    errorCheckInUrl = String.format(CHECK_IN_URL, okapiPort, REMOTE_STORAGE_ERROR_CONFIGURATION_ID);
   }
 
   @AfterEach
@@ -44,5 +50,12 @@ public class CheckInRetrieveTest extends TestBase {
     var itemBarcode = "{\"itemBarcode\": \"2887532577331\"}";
     var response = post(checkInUrl, itemBarcode, String.class);
     assertThat(response.getStatusCode(), is(HttpStatus.OK));
+  }
+
+  @Test
+  void shouldReturnBadRequestForInvalidRemoteStorageId() {
+    var itemBarcode = "{\"itemBarcode\": \"2887532577331\"}";
+    HttpClientErrorException exception = assertThrows(HttpClientErrorException.class, () -> post(errorCheckInUrl, itemBarcode, String.class));
+    assertThat(exception.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
   }
 }

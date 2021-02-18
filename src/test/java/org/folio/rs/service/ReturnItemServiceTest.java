28dcf90c0fe43ffb03bb556c48b12f539715aeb9
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2021-02-18 15:40:31 +0300

    MODRS-10 - Update tests

--- src/test/java/org/folio/rs/service/ReturnItemServiceTest.java
+++ src/test/java/org/folio/rs/service/ReturnItemServiceTest.java
@@ -8,13 +8,16 @@ import org.folio.rs.domain.dto.Request;
 import org.folio.rs.domain.dto.ResultList;
 import org.folio.rs.domain.entity.RetrievalQueueRecord;
 import org.folio.rs.domain.dto.Item;
+import org.folio.rs.error.ItemReturnException;
 import org.folio.rs.repository.RetrievalQueueRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,7 +32,8 @@ import static org.mockito.Mockito.when;
 public class ReturnItemServiceTest {
 
   private static final String REMOTE_STORAGE_CONFIGURATION_ID = "de17bad7-2a30-4f1c-bee5-f653ded15629";
-
+  private static final String ITEM_BARCODE = "item_barcode";
+  private static final String ITEM_ID = "item_id";
   @Mock
   private InventoryClient inventoryClient;
   @Mock
@@ -45,11 +49,11 @@ public class ReturnItemServiceTest {
   @Test
   public void testReturnItemService() {
     var checkInItem = new CheckInItem();
-    checkInItem.setItemBarcode("item_barcode");
+    checkInItem.setItemBarcode(ITEM_BARCODE);
 
     var item = new Item();
-    item.setId("item_id");
-    item.setBarcode("item_barcode");
+    item.setId(ITEM_ID);
+    item.setBarcode(ITEM_BARCODE);
     var itemResult = new ResultList<Item>();
     itemResult.setTotalRecords(1);
     itemResult.setResult(Collections.singletonList(item));
@@ -71,5 +75,48 @@ public class ReturnItemServiceTest {
 
     verify(retrievalQueueRepository, times(1)).save(isA(RetrievalQueueRecord.class));
     assertThat(returnItemResponse.getIsHoldRecallRequestExist(), is(true));
+  }
+
+  @Test
+  public void testReturnItemServiceIfItemRequestsNotExist() {
+    var checkInItem = new CheckInItem();
+    checkInItem.setItemBarcode(ITEM_BARCODE);
+
+    var item = new Item();
+    item.setId(ITEM_ID);
+    item.setBarcode(ITEM_BARCODE);
+    var itemResult = new ResultList<Item>();
+    itemResult.setTotalRecords(1);
+    itemResult.setResult(Collections.singletonList(item));
+
+    ItemRequests itemRequests = new ItemRequests();
+    itemRequests.setRequests(new ArrayList<>());
+    itemRequests.setTotalRecords(0);
+
+    when(inventoryClient.getItemsByQuery("barcode==" + item.getBarcode())).thenReturn(itemResult);
+    when(circulationClient.getItemRequests(item.getId())).thenReturn(itemRequests);
+
+    var returnItemResponse = returnItemService.returnItem(REMOTE_STORAGE_CONFIGURATION_ID, checkInItem);
+
+    verify(retrievalQueueRepository, times(0)).save(isA(RetrievalQueueRecord.class));
+    assertThat(returnItemResponse.getIsHoldRecallRequestExist(), is(false));
+  }
+
+  @Test
+  public void testReturnItemServiceIfItemByBarcodeNotExist() {
+    var checkInItem = new CheckInItem();
+    checkInItem.setItemBarcode(ITEM_BARCODE);
+
+    var item = new Item();
+    item.setId(ITEM_ID);
+    item.setBarcode(ITEM_BARCODE);
+    var itemResult = new ResultList<Item>();
+    itemResult.setTotalRecords(0);
+    itemResult.setResult(new ArrayList<>());
+
+    when(inventoryClient.getItemsByQuery("barcode==" + item.getBarcode())).thenReturn(itemResult);
+
+    Assertions.assertThrows(ItemReturnException.class,
+      () -> returnItemService.returnItem(REMOTE_STORAGE_CONFIGURATION_ID, checkInItem));
   }
 }

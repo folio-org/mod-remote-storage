849a4d169b9f0daef8b18b5b2f5bf81d781581de
Aliaksei_Harbuz <Aliaksei_Harbuz@epam.com>
2021-02-22 15:30:32 +0300

    MODRS-10 - Remove setter for Contributor

--- src/main/java/org/folio/rs/domain/dto/Contributor.java
+++ src/main/java/org/folio/rs/domain/dto/Contributor.java
@@ -4,11 +4,9 @@ package org.folio.rs.domain.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
-@Setter
 public class Contributor {
   private String name;
 }

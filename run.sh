b098b8d597eae9cafb80c2cbc7fb51af8739a28e
Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>
2021-03-26 10:17:30 +0300

    MODRS-30 - mod-remote-storage building failure due to unit test (#59)
    * MODRS-30
    # Fixing wiremock path
    
    * MODRS-30
    # Improving Docker file
    
    (cherry picked from commit 7a22f3ca5068bb711f14711bf902ff2c7f25492f)

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 #
+#DB_PROPS_FILE=/usr/verticles/1.txt
+#touch ${DB_PROPS_FILE}
+#echo "spring.datasource.username=${DB_USERNAME} spring.datasource.password=${DB_PASSWORD} spring.datasource.url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}" | sed 's/\s\+/\n/g' | tee ${DB_PROPS_FILE}
 #
-#JAVA_OPTS=$JAVA_OPTIONS
+
+############################################################################
+# Assign fake values to variables (like DB_HOST/DB_PORT etc) if not provided
+# This way DB connection won't be established but the application boots up
+############################################################################
+
+DB_URL="jdbc:postgresql://${DB_HOST:-localhost}:${DB_PORT:-5432}/${DB_DATABASE:-db}"
 #
-DB_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
+DB_OPTS="-Dspring.datasource.username=${DB_USERNAME:-user} -Dspring.datasource.password=${DB_PASSWORD:-pass} -Dspring.datasource.url=${DB_URL}"
 #
-APP_OPTS="--spring.datasource.username=${DB_USERNAME} \
-        --spring.datasource.password=${DB_PASSWORD} \
-        --spring.datasource.url=${DB_URL}"
-#
-#
-echo "APP_OPTS=${APP_OPTS}"
-#
-exec java org.springframework.boot.loader.JarLauncher ${APP_OPTS}
+export JAVA_OPTIONS="${JAVA_OPTIONS:-} ${DB_OPTS}"

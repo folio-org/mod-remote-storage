383aee0cb1f3edcf82b5e273ab200b9a1da72ce6
siarhei-charniak <siarhei_charniak@epam.com>
2021-04-04 19:08:58 +0300

    MODRS-31 - Standard health check endpoint (#62)
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31 - Standard health check endpoint
    
    * MODRS-31
    # /admin/health
    
    * MODRS-31
    # /admin/health
    
    * MODRS-31
    # /admin/health
    
    Co-authored-by: Viachaslau Khandramai <Viachaslau_Khandramai@epam.com>

--- src/main/java/org/folio/rs/ModRemoteStorageApplication.java
+++ src/main/java/org/folio/rs/ModRemoteStorageApplication.java
@@ -1,6 +1,7 @@
 package org.folio.rs;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients
+@EnableAutoConfiguration
 public class ModRemoteStorageApplication {
   public static void main(String[] args) {
     SpringApplication.run(ModRemoteStorageApplication.class, args);

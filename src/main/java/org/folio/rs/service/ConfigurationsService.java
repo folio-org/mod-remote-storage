031dc23d81148a9475e26ec187ed7cd80066683a
Vitalii_Zahoruiko <vitalii_zahoruiko@epam.com>
2021-04-22 14:17:55 +0300

    MODRS-45 - Remove accession workflow preference from Configuration

--- src/main/java/org/folio/rs/service/ConfigurationsService.java
+++ src/main/java/org/folio/rs/service/ConfigurationsService.java
@@ -78,7 +78,6 @@ public class ConfigurationsService {
     dest.setAccessionTimeUnit(source.getAccessionTimeUnit());
     dest.setUpdatedByUserId(source.getUpdatedByUserId());
     dest.setUpdatedByUsername(source.getUpdatedByUsername());
-    dest.setAccessionWorkflowDetails(source.getAccessionWorkflowDetails());
     dest.setReturningWorkflowDetails(source.getReturningWorkflowDetails());
     dest.setUpdatedDate(LocalDateTime.now());
     return dest;

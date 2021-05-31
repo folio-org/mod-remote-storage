b058215175510fafcaad3c58ecc7cb086a35070c
Siarhei Charniak <siarhei_charniak@epam.com>
2021-05-31 12:16:48 +0300

    MODRS-50 Improvements - Fixed post extended mapping issue

--- src/test/java/org/folio/rs/controller/LocationMappingsTest.java
+++ src/test/java/org/folio/rs/controller/LocationMappingsTest.java
@@ -6,7 +6,6 @@ import static org.folio.rs.util.Utils.randomIdAsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -149,8 +148,6 @@ public class LocationMappingsTest extends TestBase {
       .getFinalLocationId(), notNullValue());
     assertThat(responseEntity.getBody()
       .getRemoteConfigurationId(), notNullValue());
-
-    assertThat(existingMapping.getRemoteConfigurationId(), not(equalTo(responseEntity.getBody().getRemoteConfigurationId())));
 
     var mapping = get(extendedMappingsUrl + "/" + responseEntity.getBody().getFinalLocationId(),
       ExtendedRemoteLocationConfigurationMappings.class).getBody();

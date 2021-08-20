bfe64838951121d8810aa9387d661d412d780b13
Siarhei Charniak <siarhei_charniak@epam.com>
2021-08-20 09:15:14 +0300

    MODRS-81 - Tenant deletion API does not purge database schema

--- src/test/java/org/folio/rs/controller/TenantControllerTest.java
+++ src/test/java/org/folio/rs/controller/TenantControllerTest.java
@@ -14,14 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("classpath:application-test.yml")
-@ActiveProfiles("TestDB")
-@EnableTransactionManagement
 @AutoConfigureEmbeddedDatabase(beanName = "dataSource")
 public class TenantControllerTest {
   private final static String DROP_SCHEMA_QUERY = "DROP SCHEMA IF EXISTS %1$s CASCADE; DROP ROLE IF EXISTS %1$s";

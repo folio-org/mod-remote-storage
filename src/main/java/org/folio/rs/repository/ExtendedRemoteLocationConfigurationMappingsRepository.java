7621b68302b882ec4d40e917173ced6831d1b0c2
siarhei-charniak <siarhei_charniak@epam.com>
2021-09-18 10:47:05 +0300

    MODRS-89 - Juniper Hotfix #3 - Item status becomes ‘In Transit’ when item returns to the remote storage (#125)

--- src/main/java/org/folio/rs/repository/ExtendedRemoteLocationConfigurationMappingsRepository.java
+++ src/main/java/org/folio/rs/repository/ExtendedRemoteLocationConfigurationMappingsRepository.java
@@ -5,11 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ExtendedRemoteLocationConfigurationMappingsRepository extends JpaRepository<RemoteLocationConfigurationMappingEntity, UUID>,
   JpaSpecificationExecutor<RemoteLocationConfigurationMappingEntity> {
-  Optional<RemoteLocationConfigurationMappingEntity> findByRemoteStorageConfigurationId(UUID remoteStorageConfigurationId);
+  List<RemoteLocationConfigurationMappingEntity> findAllByRemoteStorageConfigurationId(UUID remoteStorageConfigurationId);
 }
--- src/main/java/org/folio/rs/service/CheckInItemService.java
+++ src/main/java/org/folio/rs/service/CheckInItemService.java
@@ -4,14 +4,18 @@ import static java.lang.String.format;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.folio.rs.client.CirculationClient;
+import org.folio.rs.client.InventoryClient;
 import org.folio.rs.client.LocationClient;
 import org.folio.rs.domain.dto.CheckInCirculationRequest;
 import org.folio.rs.domain.dto.CheckInItem;
 import org.folio.rs.domain.dto.CheckInItemByHoldId;
+import org.folio.rs.domain.entity.RemoteLocationConfigurationMappingEntity;
 import org.folio.rs.error.CheckInException;
 import org.folio.rs.repository.ExtendedRemoteLocationConfigurationMappingsRepository;
 import org.springframework.stereotype.Service;
@@ -29,23 +33,15 @@ public class CheckInItemService {
   private final ExtendedRemoteLocationConfigurationMappingsRepository mappingsRepository;
   private final LocationClient locationClient;
   private final ReturnRetrievalQueueService returnRetrievalQueueService;
+  private final InventoryClient inventoryClient;
 
   public void checkInItemByBarcode(String remoteStorageConfigurationId, CheckInItem checkInItem) {
     log.info("Start check-in process for item with barcode " + checkInItem.getItemBarcode());
-    var locationMapping = mappingsRepository.findByRemoteStorageConfigurationId(UUID.fromString(remoteStorageConfigurationId));
-    if (locationMapping.isEmpty()) {
-      throw new CheckInException("Folio location does not exist for remoteStorageConfigurationId " + remoteStorageConfigurationId);
-    } else {
-      var finalLocationId = locationMapping.get().getFinalLocationId().toString();
-      var folioLocation = locationClient.getLocation(finalLocationId);
-      if (StringUtils.isBlank(folioLocation.getPrimaryServicePoint())) {
-        throw new CheckInException("Primary service point is empty for remoteStorageConfigurationId " + remoteStorageConfigurationId);
-      } else {
-        circulationClient.checkIn(CheckInCirculationRequest.of(checkInItem.getItemBarcode(),
-          folioLocation.getPrimaryServicePoint(), Instant.now().truncatedTo(ChronoUnit.MILLIS)));
-        log.info("Check-in success for item with barcode " + checkInItem.getItemBarcode());
-      }
-    }
+    var remoteLocations = getRemoteLocationsByRemoteStorageConfigurationId(remoteStorageConfigurationId);
+    var checkInLocation = getCheckInLocationId(remoteLocations, checkInItem);
+    circulationClient.checkIn(CheckInCirculationRequest.of(checkInItem.getItemBarcode(),
+      getPrimaryServicePoint(checkInLocation), Instant.now().truncatedTo(ChronoUnit.MILLIS)));
+    log.info("Check-in success for item with barcode " + checkInItem.getItemBarcode());
   }
 
   public void checkInItemByHoldId(String remoteStorageConfigurationId, CheckInItemByHoldId checkInItemByHoldId) {
@@ -59,5 +55,36 @@ public class CheckInItemService {
     var checkInItemByBarcode = new CheckInItem();
     checkInItemByBarcode.setItemBarcode(barcode);
     checkInItemByBarcode(remoteStorageConfigurationId, checkInItemByBarcode);
+  }
+
+  private List<String> getRemoteLocationsByRemoteStorageConfigurationId(String remoteStorageConfigurationId) {
+    var locations = mappingsRepository.findAllByRemoteStorageConfigurationId(UUID.fromString(remoteStorageConfigurationId)).stream()
+      .map(RemoteLocationConfigurationMappingEntity::getFinalLocationId)
+      .map(UUID::toString)
+      .collect(Collectors.toList());
+    if (locations.isEmpty()) {
+      throw new CheckInException("Folio location does not exist for remoteStorageConfigurationId " + remoteStorageConfigurationId);
+    }
+    return locations;
+  }
+
+  private String getCheckInLocationId(List<String> remoteLocations, CheckInItem checkInItem) {
+    if (remoteLocations.size() == 1) {
+      return remoteLocations.get(0);
+    }
+    var item = inventoryClient.getItemByBarcode(checkInItem.getItemBarcode());
+    if (remoteLocations.contains(item.getEffectiveLocation().getId())) {
+      return item.getEffectiveLocation().getId();
+    }
+    log.info("Item with barcode=" + item.getBarcode() + " has no matching remote location, aborting check-in");
+    throw new CheckInException("Item's location doesn't match any remote location");
+  }
+
+  private String getPrimaryServicePoint(String locationId) {
+    var location = locationClient.getLocation(locationId);
+    if (StringUtils.isBlank(location.getPrimaryServicePoint())) {
+      throw new CheckInException("Primary service point does not exist for locationId " + locationId);
+    }
+    return location.getPrimaryServicePoint();
   }
 }

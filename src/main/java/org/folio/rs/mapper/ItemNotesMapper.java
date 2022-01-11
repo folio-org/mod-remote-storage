1768e4d7ca477b2cd645acb8fddfde6f57393d58
Siarhei Charniak <siarhei_charniak@epam.com>
2022-01-11 10:35:54 +0300

    MODRS-103 - Kiwi R3 2021 - Log4j vulnerability verification and correction

--- src/main/java/org/folio/rs/mapper/ItemNotesMapper.java
+++ src/main/java/org/folio/rs/mapper/ItemNotesMapper.java
@@ -2,7 +2,9 @@ package org.folio.rs.mapper;
 
 import org.folio.rs.domain.dto.AccessionQueueNote;
 import org.folio.rs.domain.entity.ItemNoteEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface ItemNotesMapper {
   AccessionQueueNote toDto(ItemNoteEntity entity);
 }

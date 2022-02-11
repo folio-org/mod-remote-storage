87cd7ff572ee32b74bcef33bb89655587bc4244c
Viachaslau_Khandrama <Viachaslau_Khandramai@epam.com>
2022-02-11 23:34:49 +0300

    MODRS-114

--- src/main/java/org/folio/rs/integration/KafkaMessageListener.java
+++ src/main/java/org/folio/rs/integration/KafkaMessageListener.java
@@ -1,14 +1,19 @@
 package org.folio.rs.integration;
 
+import static org.folio.rs.controller.TenantController.SYSTEM_USER;
+
 import java.util.List;
 
+import org.folio.HttpStatus;
 import org.folio.rs.domain.dto.DomainEvent;
-import org.folio.rs.repository.SystemUserParametersRepository;
 import org.folio.rs.service.AccessionQueueService;
 import org.folio.rs.service.KafkaService;
+import org.folio.rs.service.SecurityManagerService;
+import org.folio.spring.FolioExecutionContext;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,16 +23,24 @@ import lombok.extern.slf4j.Slf4j;
 public class KafkaMessageListener {
 
   private final AccessionQueueService accessionQueueService;
-  private final SystemUserParametersRepository systemUserParametersRepository;
+  private final SecurityManagerService securityManagerService;
+  private final FolioExecutionContext folioExecutionContext;
 
-  @KafkaListener(
-    id = KafkaService.EVENT_LISTENER_ID,
-    containerFactory = "kafkaListenerContainerFactory",
-    topicPattern = "${application.kafka.listener.events.topic-pattern}",
-    groupId = "${application.kafka.listener.events.group-id}",
-    concurrency = "${application.kafka.listener.events.concurrency}")
+  @KafkaListener(id = KafkaService.EVENT_LISTENER_ID, containerFactory = "kafkaListenerContainerFactory", topicPattern = "${application.kafka.listener.events.topic-pattern}", groupId = "${application.kafka.listener.events.group-id}", concurrency = "${application.kafka.listener.events.concurrency}")
   public void handleEvents(List<DomainEvent> events) {
     log.info("Processing resource events from kafka [eventsCount: {}]", events.size());
-    accessionQueueService.processAccessionQueueRecord(events);
+    try {
+      accessionQueueService.processAccessionQueueRecord(events);
+    } catch (FeignException fe) {
+      if (fe.status() == HttpStatus.HTTP_UNAUTHORIZED.toInt()) {
+        log.warn("Re-authorization attempt due to: {}", fe.getMessage());
+        securityManagerService.prepareOrUpdateSystemUser(SYSTEM_USER, SYSTEM_USER, folioExecutionContext.getOkapiUrl(),
+            folioExecutionContext.getTenantId());
+        accessionQueueService.processAccessionQueueRecord(events);
+      } else {
+        log.error("Error processing Kafka event", fe);
+        throw fe;
+      }
+    }
   }
 }
---
+++ src/test/java/org/folio/rs/service/KafkaListenerTest.java
@@ -0,0 +1,101 @@
+package org.folio.rs.service;
+
+import feign.FeignException;
+import feign.Request;
+import feign.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.folio.rs.domain.dto.DomainEvent;
+import org.folio.rs.integration.KafkaMessageListener;
+import org.folio.spring.FolioExecutionContext;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+public class KafkaListenerTest {
+
+  @InjectMocks
+  private KafkaMessageListener kafkaMessageListener;
+  @Mock
+  private SecurityManagerService securityManagerService;
+  @Mock
+  private FolioExecutionContext folioExecutionContext;
+  @Mock
+  private AccessionQueueService accessionQueueService;
+
+  @Test
+  public void testExpectedFlow() {
+    log.info("======= Test Kafka events processing: Successful Case =======");
+
+    // then
+    kafkaMessageListener.handleEvents(getEventsList());
+
+    // verify
+    verify(accessionQueueService, times(1)).processAccessionQueueRecord(any());
+    verify(securityManagerService, times(0)).prepareOrUpdateSystemUser(any(), any(), any(), any());
+  }
+
+  @Test
+  public void testUnauthorizedError() {
+    log.info("======= Test Kafka events processing: Re-authorization in Authorization Error Case =======");
+    // when
+    doThrow(prepareFeignException(401)).when(accessionQueueService)
+      .processAccessionQueueRecord(any());
+    doNothing().when(securityManagerService)
+      .prepareOrUpdateSystemUser(any(), any(), any(), any());
+
+    // then
+    assertThrows(FeignException.class, () -> kafkaMessageListener.handleEvents(getEventsList()));
+
+    // verify
+    verify(accessionQueueService, times(2)).processAccessionQueueRecord(any());
+    verify(securityManagerService, times(1)).prepareOrUpdateSystemUser(any(), any(), any(), any());
+
+  }
+
+  @Test
+  public void testNonUnauthorizedError() {
+    log.info("======= Test Kafka events processing: Skipping Re-authorization in non-Authorization Error Case =======");
+
+    // when
+    doThrow(prepareFeignException(500)).when(accessionQueueService)
+      .processAccessionQueueRecord(any());
+
+    // then
+    assertThrows(FeignException.class, () -> kafkaMessageListener.handleEvents(getEventsList()));
+
+    // verify
+    verify(accessionQueueService, times(1)).processAccessionQueueRecord(any());
+    verify(securityManagerService, times(0)).prepareOrUpdateSystemUser(any(), any(), any(), any());
+
+  }
+
+  private List<DomainEvent> getEventsList() {
+    return List.of(new DomainEvent());
+  }
+
+  private FeignException prepareFeignException(int status) {
+    return FeignException.errorStatus("method-key", Response.builder()
+      .status(status)
+      .request(prepareMockRequest())
+      .build());
+  }
+
+  private Request prepareMockRequest() {
+    return Request.create(Request.HttpMethod.GET, "", new HashMap<>(), new byte[0], Charset.defaultCharset(), null);
+  }
+}

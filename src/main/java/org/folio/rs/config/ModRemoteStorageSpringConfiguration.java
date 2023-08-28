54659a21ed0404082aa6cf90a76682a08c0d846d
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-08-28 23:08:03 +0530

    MODRS-142 Implement refresh token rotation using Folio spring system user library

--- src/main/java/org/folio/rs/config/ModRemoteStorageSpringConfiguration.java
+++ src/main/java/org/folio/rs/config/ModRemoteStorageSpringConfiguration.java
@@ -3,6 +3,7 @@ package org.folio.rs.config;
 import feign.Logger;
 import org.folio.rs.client.EnrichHeadersClient;
 import org.folio.rs.client.logger.SensitiveDataProtectionLogger;
+import org.folio.spring.service.SystemUserProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,5 +20,9 @@ public class ModRemoteStorageSpringConfiguration {
   @Bean
   public Logger feignLogger() {
     return new SensitiveDataProtectionLogger();
+  }
+  @Bean
+  public SystemUserProperties systemUserProperties() {
+    return new SystemUserProperties("system-user","system-user","System","permissions/system-user-permissions.csv");
   }
 }

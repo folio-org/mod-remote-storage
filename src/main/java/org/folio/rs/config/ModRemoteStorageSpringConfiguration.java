2cb19391bee628d26bb071945acf7ba836e0c552
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-08-31 21:37:39 +0530

    MODRS-142 Remove the bean

--- src/main/java/org/folio/rs/config/ModRemoteStorageSpringConfiguration.java
+++ src/main/java/org/folio/rs/config/ModRemoteStorageSpringConfiguration.java
@@ -3,7 +3,6 @@ package org.folio.rs.config;
 import feign.Logger;
 import org.folio.rs.client.EnrichHeadersClient;
 import org.folio.rs.client.logger.SensitiveDataProtectionLogger;
-import org.folio.spring.service.SystemUserProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,8 +20,5 @@ public class ModRemoteStorageSpringConfiguration {
   public Logger feignLogger() {
     return new SensitiveDataProtectionLogger();
   }
-  @Bean
-  public SystemUserProperties systemUserProperties() {
-    return new SystemUserProperties("system-user","system-user","System","permissions/system-user-permissions.csv");
-  }
+
 }

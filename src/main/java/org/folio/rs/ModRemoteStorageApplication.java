7d0c5810b2f5cef65ab44f046a9997c7aa6e950f
Kapil Verma <kapil_verma1@epam.com>
2024-09-02 15:07:46 +0530

    MODRS-204: removed SYSTEM_USER_PASSWORD check from main file

--- src/main/java/org/folio/rs/ModRemoteStorageApplication.java
+++ src/main/java/org/folio/rs/ModRemoteStorageApplication.java
@@ -1,6 +1,5 @@
 package org.folio.rs;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableAutoConfiguration
 public class ModRemoteStorageApplication {
-  public static final String SYSTEM_USER_PASSWORD = "SYSTEM_USER_PASSWORD";
   public static void main(String[] args) {
-    if (StringUtils.isEmpty(System.getenv(SYSTEM_USER_PASSWORD))) {
-      throw new IllegalArgumentException("Required environment variable is missing: " + SYSTEM_USER_PASSWORD);
-    }
     SpringApplication.run(ModRemoteStorageApplication.class, args);
   }
 }
